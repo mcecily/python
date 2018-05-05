077606772843c673f79e19e8dc8e975178c726a7
mcecily <37623272+mcecily@users.noreply.github.com>
2018-05-05 15:35:26 +0300

    Add files via upload

---
+++ rps.py
@@ -0,0 +1,10 @@
+print"truth table"
+print "player 1"
+print "player 2"
+if (player1 == 'rock' and player2 == 'rock'):
+    print"both player 1 and player 2 wins"
+if (player1 == 'rock'and player2 == 'scissors'):
+    print"player 1 wins"
+else:
+    print"This is not a valid object selection"
+
